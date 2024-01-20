4e184f592dc41a6ee1ca97170af1c55119d701be
Samuel Chen <sam@dhcp-10-29-156-154.dyn.MIT.EDU>
2024-01-19 21:45:20 -0500

    main changes

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -5,7 +5,8 @@ import "../../utilities.css";
 import "./Skeleton.css";
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
-const GOOGLE_CLIENT_ID = "https://docs.google.com/document/d/110JdHAn3Wnp3_AyQLkqH2W8h5oby7OVsYIeHYSiUzRs/edit?usp=sharing";
+const GOOGLE_CLIENT_ID =
+  "https://docs.google.com/document/d/110JdHAn3Wnp3_AyQLkqH2W8h5oby7OVsYIeHYSiUzRs/edit?usp=sharing";
 
 const Skeleton = ({ userId, handleLogin, handleLogout }) => {
   return (
@@ -22,25 +23,27 @@ const Skeleton = ({ userId, handleLogin, handleLogout }) => {
       ) : (
         <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
       )}
-      <h1>Good luck on your project :)</h1>
-      <h2> What you need to change in this skeleton</h2>
-      <ul>
-        <li>
-          Change the Frontend CLIENT_ID (Skeleton.js) to your team's CLIENT_ID (obtain this at
-          http://weblab.is/clientid)
-
-        </li>
-        <li>Change the Server CLIENT_ID to the same CLIENT_ID (auth.js)</li>
-        <li>
-          Change the Database SRV (mongoConnectionURL) for Atlas (server.js). You got this in the
-          MongoDB setup.
-        </li>
-        <li>Change the Database Name for MongoDB to whatever you put in the SRV (server.js)</li>
-      </ul>
-      <h2>How to go from this skeleton to our actual app</h2>
-      <a href="https://docs.google.com/document/d/110JdHAn3Wnp3_AyQLkqH2W8h5oby7OVsYIeHYSiUzRs/edit?usp=sharing">
-        Check out this getting started guide
-      </a>
+      <body>
+        <div class="banner"></div>
+        <h1>AI Spyfall</h1>
+        <h2> Do you have what it takes to be a millionaire? </h2>
+        <ul>
+          <li>
+            Change the Frontend CLIENT_ID (Skeleton.js) to your team's CLIENT_ID (obtain this at
+            http://weblab.is/clientid)
+          </li>
+          <li>Change the Server CLIENT_ID to the same CLIENT_ID (auth.js)</li>
+          <li>
+            Change the Database SRV (mongoConnectionURL) for Atlas (server.js). You got this in the
+            MongoDB setup.
+          </li>
+          <li>Change the Database Name for MongoDB to whatever you put in the SRV (server.js)</li>
+        </ul>
+        <h2>How to go from this skeleton to our actual app</h2>
+        <a href="https://docs.google.com/document/d/110JdHAn3Wnp3_AyQLkqH2W8h5oby7OVsYIeHYSiUzRs/edit?usp=sharing">
+          Check out this getting started guide
+        </a>
+      </body>
     </GoogleOAuthProvider>
   );
 };
