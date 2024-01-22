e311703120a612b31e0adcc40a04c01b38013fa5
Samuel Chen <sam@dhcp-10-29-156-154.dyn.MIT.EDU>
2024-01-22 02:29:20 -0500

    hello

--- client/src/components/pages/GamePage.js
+++ client/src/components/pages/GamePage.js
@@ -4,6 +4,9 @@ import background_photo from "../../public/gameroom_background.png";
 import player1 from "../../public/player1.png";
 import player2 from "../../public/player2.png";
 import player3 from "../../public/player3.png";
+import dice_rolling from "../../public/DiceRolling.gif";
+import "../modules/Dice.js";
+import "../modules/Dice.css";
 
 const GamePage = () => {
   return (
@@ -20,6 +23,12 @@ const GamePage = () => {
       <img src={player1} className="player1" />;
       <img src={player2} className="player2" />;
       <img src={player3} className="player3" />;
+      <img src={dice_rolling} className="rollingDice" />;
+      <div id="dice-container">
+        <canvas id="canvas"></canvas>
+        <div id="score-result"></div>
+        <button id="roll-btn">Roll Dice</button>
+      </div>
     </div>
   );
 };
