d785814bb7c933a024c7bcfa384d3871d0404015
Samuel Chen <sam@dhcp-10-29-156-154.dyn.MIT.EDU>
2024-01-21 23:02:48 -0500

    hello

--- client/src/components/pages/GamePage.js
+++ client/src/components/pages/GamePage.js
@@ -1,19 +1,26 @@
 import React from "react";
-import "./GamePage.css"
-import background_photo from "../../public/Background.jpg";
+import "./GamePage.css";
+import background_photo from "../../public/gameroom_background.png";
+import player1 from "../../public/player1.png";
+import player2 from "../../public/player2.png";
+import player3 from "../../public/player3.png";
 
 const GamePage = () => {
   return (
-    <div style={{
-      backgroundImage: `url(${background_photo})`, 
-      margin: 0,
-      width: "100%", 
-      height: "100vh",
-      backgroundRepeat: "no-repeat",
-      backgroundSize:"cover"
-    }}>
-      HIIIIIIIII
+    <div
+      style={{
+        backgroundImage: `url(${background_photo})`,
+        margin: 0,
+        width: "100%",
+        height: "100vh",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <img src={player1} className="player1" />;
+      <img src={player2} className="player2" />;
+      <img src={player3} className="player3" />;
     </div>
   );
-}
+};
 export default GamePage;
