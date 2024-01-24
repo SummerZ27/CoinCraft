96e20fc9cf9667caf7c39bbee458c2297d04bf76
SummerZ27 <130198471+SummerZ27@users.noreply.github.com>
2024-01-24 14:25:05 -0500

    text box

--- client/src/components/pages/GamePage.js
+++ client/src/components/pages/GamePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./GamePage.css";
 import background_photo from "../../public/gameroom_background.png";
 import player1 from "../../public/players.png";
@@ -10,6 +10,16 @@ import DiceRoller from "../modules/Dice.js";
 
 const GamePage = ({ userName }) => {
   console.log(userName);
+  const [inputText, setInputText] = useState("");
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+  const handleSubmit = () => {
+    // Do something with the input text, e.g., send it to a server
+    console.log("Submitted:", inputText);
+    // You can add more logic here based on your use case
+  };
+
   return (
     <div
       style={{
@@ -25,15 +35,22 @@ const GamePage = ({ userName }) => {
       <img src={player2} className="player2" />;
       <img src={player3} className="player3" />;
       <img src={player4} className="player4" />;
-      <body>
-        <div className="content">
-          <canvas id="canvas"></canvas>
-          <div className="ui-controls">
-            <button id="roll-btn">Throw the dice</button>
-          </div>
+      <div className="content">
+        <canvas id="canvas"></canvas>
+        <div className="ui-controls">
+          <button id="roll-btn">Throw the dice</button>
         </div>
-        <DiceRoller />
-      </body>
+      </div>
+      <DiceRoller />
+      <div className="user-input">
+        <input
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder="Describe your phrase..."
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
     </div>
   );
 };
